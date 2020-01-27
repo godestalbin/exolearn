bd14200d16fd294caf26992b25d5ca97ea04fc09
GUY OLIVIER DE SAINT ALBIN <20000263>
2020-01-27 13:31:36 +0100

    Added missing comma

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/' methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("main_page.html")
