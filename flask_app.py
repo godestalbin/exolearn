68788d13e6196dc23a22b8bf180230043b934bbb
GUY OLIVIER DE SAINT ALBIN <20000263>
2020-01-30 13:12:04 +0100

    Added config for DB

--- flask_app.py
+++ flask_app.py
@@ -2,16 +2,34 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_app import db
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="godestalbin2",
+    password="tZW2u6xjdXzKPvs",
+    hostname="godestalbin2.mysql.eu.pythonanywhere-services.com",
+    databasename="godestalbin2$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 comments = []
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("main_page.html", comments=comments)
-
+    
     comments.append(request.form["contents"])
     return redirect(url_for('index'))
