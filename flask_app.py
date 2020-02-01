c99368844a426d13adf0c86907e2e165df557c6f
godestalbin <godestalbin@gmail.com>
2020-02-01 15:30:03 +0100

    Added login page

--- flask_app.py
+++ flask_app.py
@@ -33,3 +33,7 @@ def index():
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route("/login/")
+def login():
+    return render_template("login_page.html")
